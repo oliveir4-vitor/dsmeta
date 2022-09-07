5f5788302fc5527bf59b5d477a7a04d8a23dff88
oliveir4-vitor <vitoraquino93@hotmail.com>
2022-09-07 10:17:40 -0400

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css'
+import logo from "../../assets/img/logo.svg"
+
+
+function Header() {
+    return(
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="dsmeta"/>
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por 
+                    <a href='#'>@devsuperior</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
